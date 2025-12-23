9a1744878d2749c29d594ec4bffefb89133e1235
danielf <danielf@potatostack.local>
2025-12-23 18:07:28 +0100

    modify syncthing

--- light/init-db.sh
+++ light/init-db.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 # PostgreSQL initialization script for PotatoStack Light
-# Creates databases and users for Immich and Seafile
+# Creates databases and users for Immich
 set -e
 
 # Validate environment variables
 if [ -z "$IMMICH_DB_PASSWORD" ]; then
   echo "Error: IMMICH_DB_PASSWORD environment variable is not set"
-  exit 1
-fi
-
-if [ -z "$SEAFILE_DB_PASSWORD" ]; then
-  echo "Error: SEAFILE_DB_PASSWORD environment variable is not set"
   exit 1
 fi
 
@@ -38,26 +33,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EO
   GRANT ALL ON ALL FUNCTIONS IN SCHEMA vectors TO immich;
   ALTER DATABASE immich OWNER TO immich;
   ALTER SCHEMA public OWNER TO immich;
-EOSQL
-
-# Create Seafile user and databases (Seafile requires 3 databases)
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOSQL
-  DO \$\$
-  BEGIN
-    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'seafile') THEN
-      CREATE ROLE seafile LOGIN PASSWORD '$SEAFILE_DB_PASSWORD';
-    END IF;
-  END
-  \$\$;
-
-  SELECT 'CREATE DATABASE ccnet_db OWNER seafile'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ccnet_db')\gexec
-
-  SELECT 'CREATE DATABASE seafile_db OWNER seafile'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'seafile_db')\gexec
-
-  SELECT 'CREATE DATABASE seahub_db OWNER seafile'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'seahub_db')\gexec
 EOSQL
 
 echo "Database initialization completed successfully"
--- light/init-db.sql
+++ light/init-db.sql
@@ -1,6 +1,6 @@
 -- PostgreSQL initialization script for PotatoStack Light
--- Creates databases and users for Immich and Seafile
--- Environment variables IMMICH_DB_PASSWORD and SEAFILE_DB_PASSWORD must be set
+-- Creates databases and users for Immich
+-- Environment variable IMMICH_DB_PASSWORD must be set
 
 -- Create Immich user
 DO $$
@@ -20,31 +20,6 @@ $$;
 -- Create Immich database
 SELECT 'CREATE DATABASE immich OWNER immich'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'immich')\gexec
-
--- Create Seafile user
-DO $$
-DECLARE
-    seafile_password text := nullif(current_setting('SEAFILE_DB_PASSWORD', true), '');
-BEGIN
-  IF seafile_password IS NULL THEN
-    RAISE EXCEPTION 'SEAFILE_DB_PASSWORD environment variable is not set';
-  END IF;
-
-  IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'seafile') THEN
-    EXECUTE format('CREATE ROLE seafile LOGIN PASSWORD %L', seafile_password);
-  END IF;
-END
-$$;
-
--- Create Seafile databases (Seafile needs 3 databases)
-SELECT 'CREATE DATABASE ccnet_db OWNER seafile'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'ccnet_db')\gexec
-
-SELECT 'CREATE DATABASE seafile_db OWNER seafile'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'seafile_db')\gexec
-
-SELECT 'CREATE DATABASE seahub_db OWNER seafile'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'seahub_db')\gexec
 
 -- Enable pgvecto.rs extension for Immich (vector search for photos)
 \connect immich;
